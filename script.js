5d3599741a1073868d741b181b8674eb617995c5
Patricio Villarroel <pat.villarroel@duocuc.cl>
2022-11-15 22:43:31 -0300

    asd

--- script.js
+++ script.js
@@ -82,6 +82,9 @@
         } else if (j == 2) {
           td.innerHTML = '<img src="img/class/' + datos[i][1].replace(/\s/g, "").toLowerCase() + '.png" alt="' + datos[i][1] + '" width="25" height="25">';
           td.appendChild(document.createTextNode(" "+datos[i][j-1]));
+        } else if (j == 3) {
+          td.innerHTML = '<img src="img/spec/' + datos[i][1].replace(/\s/g, "").toLowerCase() +"/"+ datos[i][2].replace(/\s/g, "").toLowerCase() + '.png" alt="' + datos[i][2] + '" width="25" height="25">';
+          td.appendChild(document.createTextNode(" "+datos[i][j-1]));
         } else if (j == 6) {
           var a = document.createElement('a');  //crea el link
           a.setAttribute('href', datos[i][j-1]);
